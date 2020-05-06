cb9314ca96ec15b061736f4465f5be8676bfeafa
FloLins <florian.linsner@rub.de>
2020-05-06 20:57:14 +0200

    Added DropOut Experiment

---
+++ experiments/local_experiments/examples/MNISTtorchDropOut.py
@@ -0,0 +1,47 @@
+import sys
+
+sys.path.append("../../../../../dlapplication")
+sys.path.append("../../../../../dlplatform")
+
+from environments.local_environment import Experiment
+from environments.datasources import FileDataSourceFactory
+from environments.datasources.dataDecoders.pytorchDataDecoders import MNISTDecoder
+from dlutils.models.pytorch.dropOutNetwork import DropoutNet
+from DLplatform.synchronizing import PeriodicSync
+from DLplatform.aggregating import Average
+from DLplatform.learning.factories.pytorchLearnerFactory import PytorchLearnerFactory
+from DLplatform.stopping import MaxAmountExamples
+from DLplatform.coordinator import InitializationHandler
+
+# import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+if __name__ == "__main__":
+    executionMode = 'cpu'  # or 'gpu'
+
+    messengerHost = 'localhost'
+    messengerPort = 5672
+    numberOfNodes = 1
+    updateRule = "SGD"
+    learningRate = 0.25
+    learningParams = {}
+    lossFunction = "CrossEntropyLoss"
+    batchSize = 8
+    delta = None
+    sync = PeriodicSync()
+    syncPeriod = 1
+
+    aggregator = Average()
+    stoppingCriterion = MaxAmountExamples(2800)
+    dsFactory = FileDataSourceFactory(filename="../../../../data/textualMNIST/mnist_train.txt", decoder=MNISTDecoder(),
+                                      numberOfNodes=numberOfNodes, indices='roundRobin', shuffle=False, cache=False)
+    learnerFactory = PytorchLearnerFactory(network=DropoutNet(), updateRule=updateRule, learningRate=learningRate,
+                                           learningParams=learningParams, lossFunction=lossFunction,
+                                           batchSize=batchSize, syncPeriod=syncPeriod)
+    initHandler = InitializationHandler()
+
+    exp = Experiment(executionMode=executionMode, messengerHost=messengerHost, messengerPort=messengerPort,
+                     numberOfNodes=numberOfNodes, sync=sync,
+                     aggregator=aggregator, learnerFactory=learnerFactory,
+                     dataSourceFactory=dsFactory, stoppingCriterion=stoppingCriterion, initHandler=initHandler)
+    exp.run("MNISTtorchCNNwithDropOut")
